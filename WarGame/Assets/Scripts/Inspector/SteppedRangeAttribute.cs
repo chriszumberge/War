4fedd5f63abdf12752ae8e803b2a45db81d89b72
Christopher Zumberge <chriszumberge@gmail.com>
2018-12-21 16:15:39 -0500

    changes for ios build (mostly image format types)

--- WarGame/Assets/Scripts/Inspector/SteppedRangeAttribute.cs
+++ WarGame/Assets/Scripts/Inspector/SteppedRangeAttribute.cs
@@ -1,17 +1,17 @@
-﻿using System;
-using UnityEngine;
+﻿//using System;
+//using UnityEngine;
 
-[AttributeUsage(AttributeTargets.Field, Inherited = true, AllowMultiple = false)]
-public class SteppedRangeAttribute : PropertyAttribute
-{
-    public readonly float min;
-    public readonly float max;
-    public readonly float step;
+//[AttributeUsage(AttributeTargets.Field, Inherited = true, AllowMultiple = false)]
+//public class SteppedRangeAttribute : PropertyAttribute
+//{
+//    public readonly float min;
+//    public readonly float max;
+//    public readonly float step;
 
-    public SteppedRangeAttribute(float min, float max, float step)
-    {
-        this.min = min;
-        this.max = max;
-        this.step = step;
-    }
-}
+//    public SteppedRangeAttribute(float min, float max, float step)
+//    {
+//        this.min = min;
+//        this.max = max;
+//        this.step = step;
+//    }
+//}
--- WarGame/Assets/Scripts/Inspector/SteppedRangeDrawer.cs
+++ WarGame/Assets/Scripts/Inspector/SteppedRangeDrawer.cs
@@ -1,37 +1,37 @@
-﻿using UnityEngine;
-using UnityEditor;
+﻿//using UnityEngine;
+//using UnityEditor;
 
-[CustomPropertyDrawer(typeof(SteppedRangeAttribute))]
-public class SteppedRangeDrawer : PropertyDrawer
-{
-    private float value;
+//[CustomPropertyDrawer(typeof(SteppedRangeAttribute))]
+//public class SteppedRangeDrawer : PropertyDrawer
+//{
+//    private float value;
 
-    public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
-    {
-        var rangeAttribute = (SteppedRangeAttribute)base.attribute;
+//    public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+//    {
+//        var rangeAttribute = (SteppedRangeAttribute)base.attribute;
 
-        if (property.propertyType == SerializedPropertyType.Float)
-        {
-            value = EditorGUI.Slider(position, label.text, value, rangeAttribute.min, rangeAttribute.max);
+//        if (property.propertyType == SerializedPropertyType.Float)
+//        {
+//            value = EditorGUI.Slider(position, label.text, value, rangeAttribute.min, rangeAttribute.max);
 
-            if (value % rangeAttribute.step != 0)
-            {
-                float lowerStep = ((int)(value / rangeAttribute.step) * rangeAttribute.step) + rangeAttribute.min;
-                float upperStep = (lowerStep + rangeAttribute.step) > rangeAttribute.max ? rangeAttribute.max : lowerStep + rangeAttribute.step;
+//            if (value % rangeAttribute.step != 0)
+//            {
+//                float lowerStep = ((int)(value / rangeAttribute.step) * rangeAttribute.step) + rangeAttribute.min;
+//                float upperStep = (lowerStep + rangeAttribute.step) > rangeAttribute.max ? rangeAttribute.max : lowerStep + rangeAttribute.step;
 
-                bool closerToLower = value - lowerStep < upperStep - value;
+//                bool closerToLower = value - lowerStep < upperStep - value;
 
-                if (closerToLower)
-                    value = lowerStep;
-                else
-                    value = upperStep;
-            }
+//                if (closerToLower)
+//                    value = lowerStep;
+//                else
+//                    value = upperStep;
+//            }
 
-            property.floatValue = value;
-        }
-        else
-        {
-            EditorGUI.LabelField(position, label.text, "Use Range with float or int.");
-        }
-    }
-}
+//            property.floatValue = value;
+//        }
+//        else
+//        {
+//            EditorGUI.LabelField(position, label.text, "Use Range with float or int.");
+//        }
+//    }
+//}
